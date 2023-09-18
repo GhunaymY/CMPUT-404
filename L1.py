2f8b596c20f4c039558c5171987b86d781a7b388
GhunaymY <116463354+GhunaymY@users.noreply.github.com>
2023-09-18 12:45:56 -0600

    Update L1.py

--- L1.py
+++ L1.py
@@ -5,4 +5,4 @@
 import requests
 print(requests.__version__)
 print(requests.get("http://www.google.com"))
-print(requests.get("https://raw.githubusercontent.com/GhunaymY/CMPUT-404/main/L1.py"))
+print(requests.get("https://raw.githubusercontent.com/GhunaymY/CMPUT-404/main/L1.py").text)
